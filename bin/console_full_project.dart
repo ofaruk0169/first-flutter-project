6f5efa5c3abdb9f5669abac59fb1d65995417717
Omare Faruk <ofaruk0169@gmail.com>
2022-11-16 19:33:23 +0000

    second commit

--- bin/console_full_project.dart
+++ bin/console_full_project.dart
@@ -1,15 +1,11 @@
-import 'package:console_full_project/console_full_project.dart' as console_full_project;
+import 'package:console_full_project/console_full_project.dart'
+    as console_full_project;
 
 void main(List<String> arguments) {
   try {
-    print('Hello world: ${console_full_project.calculate()}! \n'
-      'Args sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse
-      (element))}');
-
-    } catch (e) {
-
-      print('Error: At least one argument is not an integer!');
-
-    }
+    print('Hello world :): ${console_full_project.calculate()}! \n'
+        'Args sum: ${arguments.fold<int>(0, (previousValue, element) => previousValue + int.parse(element))}');
+  } catch (e) {
+    print('Error: At least one argument is not an integer!');
   }
-  
+}
